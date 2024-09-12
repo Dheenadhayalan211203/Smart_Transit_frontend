e0658fdce8a3a25262b571386a00ce73fdc08519
Dheenadhayalan211203 <dheenashobha2k3@gmail.com>
2024-09-12 20:26:11 +0530

    added the files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Counters from './Counter';
+import Ticketbooked from './Ticketbooked';
+import Dashboard from './Dashboard';
+import Home from './Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Route for Counters component */}
+        <Route path="/counter" element={<Counters />} />
+        
+        {/* Route for Ticketbooked component */}
+        <Route path="/ticketbooked" element={<Ticketbooked />} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Ticketbooked.jsx
@@ -0,0 +1,13 @@
+const Ticketbooked=()=>
+{
+    return(
+        <div className="ticketbooked">
+            <h1>Booking Successfull</h1>
+            <img src="tick.gif" alt="" />
+            <h2> Verified</h2>
+
+        </div>
+    )
+}
+
+export default Ticketbooked
